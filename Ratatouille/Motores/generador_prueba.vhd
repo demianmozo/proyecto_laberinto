6399b8368c189b6219798ad54a0dd48ebdae177c
demianmozo <demianmozo@gmail.com>
2024-11-14 19:38:49 -0300

    Resiliencia: capacidad para adaptarse a las situaciones adversas con resultados positivos.

---
+++ Ratatouille/Motores/generador_prueba.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity generador_prueba is
+    port (
+        prueba : out std_logic_vector(1 downto 0)  -- Señal de salida de 2 bits
+    );
+end generador_prueba;
+
+architecture Behavioral of generador_prueba is
+begin
+    -- Asignación concurrente
+    prueba <= "10";  -- Asigna el valor binario '10' (equivalente a 2 en decimal)
+    
+end Behavioral;
